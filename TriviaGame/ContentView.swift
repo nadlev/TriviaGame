9fcdc0a2206eda9c6d5dee0243c701a57e883c6c
nadlev <nadiya.levytska.jr@gmail.com>
2023-01-17 12:01:01 +0200

    Initial Commit

---
+++ TriviaGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TriviaGame
+//
+//  Created by Надежда Левицкая on 1/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Надежда Левицкая on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
